85f99253509f65e5acf867b7b48ac8848cb9d217
Toukom's <tokiniaina090605@gmail.com>
2024-08-27 19:32:06 +0300

    fix: page build about generateStaticProps.

--- src/app/explore/[id]/page.tsx
+++ src/app/explore/[id]/page.tsx
@@ -1,84 +1,18 @@
-"use client";
-import { Skeleton } from "@/components/ui/skeleton";
+import BookDetail from "@/components/ui/custom/book-detail";
 import { books } from "@/constant/books";
-import { cn } from "@/lib/utils";
-import { MoveUpRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { notFound } from "next/navigation";
-import { useEffect, useState } from "react";
 
-export function generateStaticProps() {
+export function generateStaticParams() {
   return books.map(book => book.id)
 }
 
-const BookDetailPage = ({ params }: { params: { id: string } }) => {
-  const [loading, setLoading] = useState(true);
-  const [book, setBook] = useState<IBooks | undefined>(undefined);
-  const getBookDetail = (id: string) => {
-    return books.filter((book) => book.id === id)[0];
-  };
-  useEffect(() => {
-    if (!params.id) notFound();
-    const bookDetail = getBookDetail(params.id);
-    if (!bookDetail) notFound();
-    setBook(bookDetail);
-  }, [params.id]);
-
-  useEffect(() => {
-    setLoading(false);
-  }, []);
+const BookDetailPage = ({ params }: { params: { id?: string } }) => {
 
   return (
     <main className="max-w-screen-lg mx-auto mt-4">
-      <div className="flex justify-center w-2/3 gap-4 p-8 mx-auto mt-4 border shadow-md rounded-xl">
-        <div className="relative w-1/3 overflow-hidden border rounded-md aspect-square">
-          {loading ? (
-            <Skeleton className="absolute top-0 left-0 w-full h-full bg-slate-100" />
-          ) : (
-            <Image
-              src={book?.image || ""}
-              alt={book?.title || ""}
-              width={100}
-              height={100}
-              className="absolute top-0 left-0 object-cover object-center w-full h-full"
-            />
-          )}
-        </div>
-        <div className="flex flex-col w-2/3 gap-2">
-          <h1 className="text-3xl font-bold">{book?.title}</h1>
-          <div className="font-normal">
-            <p
-              className={cn("text-sm text-primary font-bold tracking-wider", {
-                "text-red-400": book?.genre === "Horror",
-                "text-yellow-400": book?.genre === "Comedy",
-                "text-blue-400": book?.genre === "Drama",
-                "text-violet-400": book?.genre === "Fantasy",
-              })}
-            >
-              {book?.genre}
-            </p>
-            <p className="text-sm text-slate-800">
-              Author: <span className="font-medium">{book?.author}</span>
-            </p>
+      <BookDetail id={params.id!} />
+      
 
-            <p className="text-xs text-slate-600">{book?.publicationDate}</p>
-          </div>
-          <p className="text-sm leading-tight text-justify text-slate-700">
-            {book?.synopsis}
-          </p>
-          <Link
-            href={book?.link || ""}
-            target="_blank"
-            className="flex items-baseline gap-1 mt-auto group hover:text-primary hover:underline w-fit"
-          >
-            Read
-            <MoveUpRight size={12} className="group-hover:scale-125" />
-          </Link>
-        </div>
-      </div>
-
-      <div className="w-2/3 p-4 mx-auto mt-4 mb-8">
+      {/* <div className="w-2/3 p-4 mx-auto mt-4 mb-8">
         <h2 className="ml-4 text-2xl font-bold">Glossary</h2>
         <ul className="flex flex-col gap-2 mt-2 ml-16 text-justify list-disc list-outside">
           <li>
@@ -110,7 +44,7 @@ const BookDetailPage = ({ params }: { params: { id: string } }) => {
             exercitationem?
           </li>
         </ul>
-      </div>
+      </div> */}
     </main>
   );
 };
---
+++ src/components/ui/custom/book-detail.tsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Skeleton } from "../skeleton";
+import { MoveUpRight } from "lucide-react";
+import { books } from "@/constant/books";
+import { notFound } from "next/navigation";
+import { Badge } from "../badge";
+import { cn } from "@/lib/utils";
+
+type StorybookDetailProps = {
+  id: string;
+};
+
+const StorybookDetail: React.FC<StorybookDetailProps> = ({ id }) => {
+  const [loading, setLoading] = useState(true);
+  const [book, setBook] = useState<IBook | undefined>(undefined);
+
+  const getBookDetail = (id: string) => {
+    return books.filter((book) => book.id === id)[0];
+  };
+
+  useEffect(() => {
+    const bookDetail = getBookDetail(id);
+    if (!bookDetail) return notFound();
+    setBook(bookDetail);
+    setLoading(false);
+  }, [id]);
+
+  return (
+    <div className="max-w-screen-md px-6 py-8 mx-auto mb-16 bg-white border rounded-lg shadow-md">
+      <div className="relative w-full h-64 mb-4">
+        {loading ? (
+          <Skeleton className="absolute top-0 left-0 w-full h-full bg-slate-100" />
+        ) : (
+          <Image
+            src={book?.image || ""}
+            alt={book?.title || ""}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-lg"
+          />
+        )}
+      </div>
+      <h2 className="mb-2 text-2xl font-bold">
+        {loading ? <Skeleton className="w-3/4 h-6" /> : book?.title}
+      </h2>
+      <Badge variant={"outline"}  className={cn("border mb-4",
+        {
+          "text-horror border-horror": book?.genre === "Horror",
+          "text-comedy border-comedy": book?.genre === "Comedy",
+          "text-fantasy border-fantasy": book?.genre === "Fantasy",
+          "text-drama border-drama": book?.genre === "Drama",
+        }
+      )}
+      >
+        {loading ? <Skeleton className="w-1/4 h-4" /> : book?.genre}
+      </Badge>
+      <div className="mb-2 text-sm text-gray-600">
+        {loading ? (
+          <Skeleton className="w-1/2 h-4" />
+        ) : (
+          `Author: ${book?.author}`
+        )}
+      </div>
+      <div className="mb-2 text-sm text-gray-500">
+        {loading ? <Skeleton className="w-1/4 h-4" /> : book?.publicationDate}
+      </div>
+      <div className="mb-4 text-gray-700">
+        {loading ? <Skeleton className="w-full h-4 mb-2" /> : book?.synopsis}
+      </div>
+      <div
+        className={`inline-block px-4 py-2 rounded hover:bg-green-600 ${
+          loading ? "bg-slate-100 text-slate-400" : "bg-green-500 text-white"
+        }`}
+      >
+        {loading ? (
+          <Skeleton className="w-16 h-4" />
+        ) : (
+          <Link
+            href={book?.link || ""}
+            target="_blank"
+            className="flex items-center gap-1"
+          >
+            Read <MoveUpRight size={12} />
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default StorybookDetail;
