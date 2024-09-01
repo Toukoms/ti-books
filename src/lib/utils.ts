107acd375d60493c6b4c74283b2ffa4d35445549
Toukom's <tokiniaina090605@gmail.com>
2024-09-01 18:37:05 +0300

    feat: add number formatter function to convert number to K format.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
+
+function nFormatter(num: number, digits: number = 2, noDec: boolean = true) {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+  const item = lookup.findLast((item) => num >= item.value);
+  return Math.abs(num) > 0
+    ? (Math.sign(num) * (Math.abs(num) / item?.value!)).toFixed(
+        Math.abs(num) < 999 && noDec ? 0 : digits
+      ) + item?.symbol
+    : "0";
+}
+
+export { cn, nFormatter };
