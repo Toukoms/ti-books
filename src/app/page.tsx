01f9d54f65c8bc616e26048b10c7b9df29040769
Toukom's <tokiniaina090605@gmail.com>
2024-09-02 19:34:00 +0300

    refactor: change title.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import FeaturedStories from "@/components/layout/featured";
 import Hero from "@/components/layout/hero";
 import HowItWorks from "@/components/layout/how-it-works";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Home"
-}
 
 export default function Home() {
   return (
